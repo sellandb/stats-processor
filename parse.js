2760c2fff380320663329a499ba2e63c0bef1b33
Ben Selland <bselland@nascar.com>
2015-11-10 16:58:06 -0500

    Added a bunch of parsing to start ingestion of Data

--- parse.js
+++ parse.js
@@ -8,15 +8,123 @@ aws.config.update({region: 'us-west-1'});
 
 var sqs = new aws.SQS();
 
-var def = Q.defer();
-
 var createQueue = Q.nbind( sqs.createQueue, sqs );
 var sendMessage = Q.nbind( sqs.sendMessage, sqs );
 
+var gameSheets = {};
+
+var messageHeader = {};
+
+//SQS Queue Setup
 var params = {
   QueueName: 'testing-queue'
 };
 
+var queueUrl = "";
+
+//Get the Game Report URLs
+
+new function(boxScoreUrl) {
+  //Go out and grab the various gamesheets
+  var def = Q.defer();
+  jsdom.env(
+    boxScoreUrl,
+    ["http://code.jquery.com/jquery.js"],
+    function (err, window) {
+      window.$("#gameReports div.contentPad li a").each(function(index, el){
+        if(el.textContent.indexOf("Game Summary") === 0){
+          gameSheets["GS"] = el.getAttribute("href");
+        }
+        if(el.textContent.indexOf("Full Play-by-Play") === 0){
+          gameSheets["PL"] = el.getAttribute("href");
+        }
+        if(el.textContent.indexOf("TOI") === 0){
+          if(el.getAttribute("href").indexOf("/TV"))
+          {
+            gameSheets["TV"] = el.getAttribute("href");
+          }
+          if(el.getAttribute("href").indexOf("/TH"))
+          {
+            gameSheets["TH"] = el.getAttribute("href");
+          }
+        }
+      });
+      def.resolve();
+    }
+  );
+  return def.promise;
+}("http://www.nhl.com/gamecenter/en/boxscore?id=2015020215")
+.then(function(){
+  //Setup the Queue URL
+
+  var def = Q.defer();
+  createQueue({QueueName: 'testing-queue'})
+  .then(function(data){
+    queueUrl = data.QueueUrl;
+    def.resolve();
+  }).catch( function(error){
+    console.log( chalk.red(err, err.stack) );// an error occurred
+    def.reject(err, err.stack);
+  });
+  return def.promise;
+
+})
+.then(function(){
+  //Process the Game Summary and store header data
+  var def = Q.defer();
+  var url = gameSheets["GS"];
+  jsdom.env(
+    url,
+    ["http://code.jquery.com/jquery.js"],
+    function (err, window) {
+      //Setup the message header
+      var year = url.replace("http://www.nhl.com/scores/htmlreports/", "").substr(0,8);
+      var gameID = url.replace("http://www.nhl.com/scores/htmlreports/" + year + "/GS02", "").substr(0,4);
+      messageHeader["year"] = year;
+      messageHeader["game"] = gameID;
+      //StdHeader #Home table td:nth-child(2)
+      var homeData = window.$("#StdHeader #Home tr:nth-child(3) td").html();
+      var homeTeam = homeData.substr(0, homeData.indexOf("<"));
+
+      var visitorData = window.$("#StdHeader #Visitor tr:nth-child(3) td").html();
+      var visitorTeam = visitorData.substr(0, visitorData.indexOf("<"));
+      console.log(visitorTeam);
+
+      //Here is the goalie data
+      /*window.$("#MainTable tr:nth-child(15) table tr.oddColor, #MainTable tr:nth-child(15) table tr.evenColor").each(function(index, el){
+        console.log(chalk.green(el.innerHTML));
+        sendMessage({
+          MessageBody: JSON.stringify({
+            date: "Monday, March 18, 2013",
+            game: "0423",
+            home: "TAMPA BAY LIGHTNING",
+            visitor: "PHILADELPHIA FLYERS",
+            type: "PL",
+            data: el.innerHTML
+          }),
+          QueueUrl: queueUrl
+        })
+        .then(function(data){
+          console.log(chalk.green(data.MessageId));
+        })
+        .catch( function(error){
+          console.log( chalk.red(err, err.stack) );// an error occurred
+        });
+      });*/
+
+      def.resolve();
+    }
+  );
+  return def.promise;
+
+});
+
+
+
+/*
+var params = {
+  QueueName: 'testing-queue'
+};
 
 createQueue({QueueName: 'testing-queue'})
 .then( function(data){
@@ -54,6 +162,32 @@ createQueue({QueueName: 'testing-queue'})
 .catch( function(error){
   console.log( chalk.red(err, err.stack) );// an error occurred
 });
+*/
+
+
+
+
+/*
+/////////////////////////////////////////////
+SCRATCH PAD
+//////////////////////////////////////////////
+*/
+
+//Data by Sheet
+//Game Summary
+//General Information (Teams, Dates, Score, Game ID)
+//Goaltender Summary
+
+//Play By Play
+//Play Information
+
+//Time on Ice Report
+// Shift length Information
+
+//Playing Roster
+// Number / Team / Position / Name
+
+
 
 /*jsdom.env(
   "http://www.nhl.com/scores/htmlreports/20122013/PL020423.HTM",
@@ -85,3 +219,9 @@ createQueue({QueueName: 'testing-queue'})
 
 //Event Row
 //table tr.evenColor
+
+//Box Score Page
+//http://www.nhl.com/gamecenter/en/boxscore?id=2012020423
+
+//Stat URLs
+//
